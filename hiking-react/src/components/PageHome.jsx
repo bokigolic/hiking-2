39ea6df7b6abb2caf6766e31a78f574a38e526b1
Bojan Golic <bokigolic32@gmail.com>
2022-02-09 16:50:45 -0500

    add dummy tours

--- hiking-react/src/components/PageHome.jsx
+++ hiking-react/src/components/PageHome.jsx
@@ -1,20 +1,27 @@
-const MojaKomponenta = () => {
-  return (
-    <p>
-      Moja komponenta
-    </p>
-  );
-}
+import { useSelector } from "react-redux";
 
+const PageHome = (props) => {
+  const tours = useSelector((state) => state.tours); // uzimamo podatak tours iz globalnog reducovog statea apliakcije
 
-const PageHome = () => {
+  let jsx = tours.map((tour, index) => {
+    return (
+      <div key={tour._id}>
+        <h4>{tour.name}</h4>
+        <div>{tour.description}</div>
+        <div>{tour.date}</div>
+        <div>{tour.trail_legth}</div>
+        <div>{tour.difficulty}</div>
+        <div>{tour.max_participants}</div>
+      </div>
+    );
+  });
+
   return (
     <>
-      <MojaKomponenta />
-      <MojaKomponenta />
-      <MojaKomponenta />
+      <h1> Home page </h1>
+      {jsx}
     </>
   );
-}
+};
 
 export default PageHome;
---
+++ hiking-react/src/utils/dummy-tours.js
@@ -0,0 +1,48 @@
+/*
+Great American Tour
+Appalachian trail
+
+Date: 01/31/2022
+Difficulty: MEDIUM
+Trail length: 10
+Max. number of participants: 10
+Average rating: 0Average rating
+*/
+
+export const dummyTours = [{
+        _id: '1',
+        name: 'Great tour',
+        description: 'Nice tour...',
+        date: '02/09/2022',
+        trail_length: 1,
+        difficulty: 'EASY',
+        max_participants: 99
+    },
+    {
+        _id: '2',
+        name: 'Virginia tour',
+        description: 'Nice tour...',
+        date: '02/09/2022',
+        trail_length: 1,
+        difficulty: 'MEDIUM',
+        max_participants: 99
+    },
+    {
+        _id: '3',
+        name: 'Amerika tour',
+        description: 'Nice tour...',
+        date: '02/09/2022',
+        trail_length: 1,
+        difficulty: 'HARD',
+        max_participants: 99
+    },
+    {
+        _id: '4',
+        name: 'Arlington tour',
+        description: 'Nice tour...',
+        date: '02/09/2022',
+        trail_length: 1,
+        difficulty: 'EASY',
+        max_participants: 99
+    },
+];
