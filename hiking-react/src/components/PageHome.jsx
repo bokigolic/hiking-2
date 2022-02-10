9e6f6d6694ac7e5d6b5f7e837960eacdd7676246
Bojan Golic <bokigolic32@gmail.com>
2022-02-10 17:18:21 -0500

    tour filter easy, medium and hard works

--- hiking-react/src/components/PageHome.jsx
+++ hiking-react/src/components/PageHome.jsx
@@ -1,9 +1,43 @@
+import { FormControlLabel, FormLabel, Radio, RadioGroup } from "@mui/material";
+import { useState } from "react";
 import { useSelector } from "react-redux";
 
 const PageHome = (props) => {
+
+  // SEKCIJA FILTER
+  const preset = {
+    search: '',
+    difficulty: 'ALL',
+    trail_length: 1,
+    max_participants: 99
+  };
+
+  const [formState, setFormState] = useState(preset);
+
+  const handleChange = (e) => {
+    // univerzalni handler za sva inpout polja
+    const target = e.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    setFormState({
+      ...formState,
+      [name]: value
+    });
+  };
+
+
+  // SEKCIJA LISTA TURA
   const tours = useSelector((state) => state.tours); // uzimamo podatak tours iz globalnog reducovog statea apliakcije
 
-  let jsx = tours.map((tour, index) => {
+  const filteredTours = tours.filter((tour)=>{
+    if (tour.difficulty === formState.difficulty || formState.difficulty === 'ALL') {
+      return true; // stay ina array
+    } else {
+      return false; // deleted from array
+    }
+  });
+
+  let jsx = filteredTours.map((tour, index) => {
     return (
       <div key={tour._id}>
         <h4>{tour.name}</h4>
@@ -19,6 +53,22 @@ const PageHome = (props) => {
   return (
     <>
       <h1> Home page </h1>
+      <h3>Filter tours</h3>
+
+      <FormLabel id="difficulty">Difficulty</FormLabel>
+            <RadioGroup
+              row
+              name="difficulty"
+              value={formState.difficulty}
+              onChange={handleChange}
+            >
+              <FormControlLabel value="ALL" control={<Radio />} label="All" />
+              <FormControlLabel value="EASY" control={<Radio />} label="Easy" />
+              <FormControlLabel value="MEDIUM" control={<Radio />} label="Medium" />
+              <FormControlLabel value="HARD" control={<Radio />} label="Hard" />
+            </RadioGroup>
+
+      <h4>Tours</h4>
       {jsx}
     </>
   );
