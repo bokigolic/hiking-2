79480cde91cd21366eecae5aba923c7f7e671581
Bojan Golic <bokigolic32@gmail.com>
2022-03-03 15:23:46 -0500

    delete works

--- hiking-express/graphql-things/root.js
+++ hiking-express/graphql-things/root.js
@@ -229,10 +229,6 @@ var root = {
       // ulogovani smo, sad mozemo da kreiramo turu
       const user_id = auth.user_id;
       // sad u bazi kreiramo turu
-
-
-
-      
       const results = await Tour.findOneAndUpdate({
         tour_id: args.tour_id,
         user_id: user_id
@@ -245,7 +241,34 @@ var root = {
         trail_length: args.trail_length,
         max_participants: args.max_participants
       });
-      
+      console.log(results);
+      return true;
+    } else {
+      // ako nismo ulogvani necemo ni da kreiramo turu
+      return false;
+    }
+  },
+
+
+  tourDelete: async (args, context) => {
+    console.log('tourDelete resolver');
+    // If context is not provided, the request object is passed as the context.
+    console.log('args');
+    console.log(args);
+    const req = context;
+    const token = req.headers[config.TOKEN_HEADER_KEY];
+    console.log(token);
+    const auth = await checkIsLoggedIn(token);
+    if (auth.is_logged_in) {
+      // ulogovani smo, sad mozemo da kreiramo turu
+      const user_id = auth.user_id;
+      // sad u bazi kreiramo turu
+      const results = await Tour.findOneAndDelete({
+        _id: args.tour_id,
+        user_id: user_id
+      });
+      // TODO: ne sme da obrise nista cak i ako id ne valja
+      console.log('mongoose results');
       console.log(results);
       return true;
     } else {
